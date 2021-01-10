f80f0baa07fe4a05e5f7f1c9ef55615c4123974c
Hugo Lesta <hlesta@edrans.com>
2021-01-10 18:45:31 -0300

    Add new functions

--- message/message.go
+++ message/message.go
@@ -1,13 +1,25 @@
 package message
 
+import "github.com/hugolesta/observer/observer"
+
 type Message struct {
-	message string
+	observers map[string]observer.Observer
+	Msg       string
 }
 
-func (m *Message) AddObserver(o Observer) {
-
+func (m *Message) AddObserver(name string, o observer.Observer) {
+	if m.observers == nil {
+		m.observers = make(map[string]observer.Observer)
+	}
+	m.observers[name] = o
 }
 
-func (m *Message) RemoveObserver(o Observer) {}
+func (m *Message) RemoveObserver(name string) {
+	delete(m.observers, name)
+}
 
-func (m *Message) NotifyObservers() {}
+func (m *Message) NotifyObservers() {
+	for _, v := range m.observers {
+		v.Notify(m.Msg)
+	}
+}
